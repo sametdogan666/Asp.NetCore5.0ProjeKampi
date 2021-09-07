e66b979700195d88e1a34894f7437a59ec971b55
Samet DOGAN <s.doqann1570@gmail.com>
2021-09-07 14:04:42 +0300

    Entity Katmaninda Writer - Comment - About - Contact Classlari Olusturuldu

---
+++ Entites/Concrete/Contact.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entites.Concrete
+{
+    public class Contact
+    {
+        public int ContactId { get; set; }
+        public string ContactUserName { get; set; }
+        public string ContactMail { get; set; }
+        public string ContactSubject { get; set; }
+        public string ContactMessage { get; set; }
+        public DateTime ContactCreateDate { get; set; }
+        public bool ContactStatus { get; set; }
+        
+    }
+}

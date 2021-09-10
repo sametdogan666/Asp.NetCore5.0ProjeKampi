99b1feab8707dcabad8080503a0333363cdeb793
Samet DOGAN <s.doqann1570@gmail.com>
2021-09-11 00:33:54 +0300

    Context Ve Migration Islemleri Yapildi.

---
+++ DataAccess/Concrete/BlogContext.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Entites.Concrete;
+using Microsoft.EntityFrameworkCore;
+
+namespace DataAccess.Concrete
+{
+    public class BlogContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(
+                "server=LAPTOP-05OJ6C2N\\SQLEXPRESS;database=CoreBlogDb; Integrated Security = true;");
+        }
+
+        public DbSet<About> Abouts { get; set; }
+        public DbSet<Blog> Blogs { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Comment> Comments { get; set; }
+        public DbSet<Contact> Contacts { get; set; }
+        public DbSet<Writer> Writers { get; set; }
+    }
+}

de56046c0de80d9eef64159ba1cd37e7f6abee3e
Samet DOGAN <s.doqann1570@gmail.com>
2021-09-11 00:13:49 +0300

    Entity Framework Core Paketleri Kuruldu

--- Entites/Concrete/About.cs
+++ Entites/Concrete/About.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +9,7 @@ namespace Entites.Concrete
 {
     public class About
     {
+        [Key]
         public int AboutId { get; set; }
         public string AboutDetails1 { get; set; }
         public string AboutImage1 { get; set; }

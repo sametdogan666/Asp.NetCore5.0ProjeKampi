0aa9706620311d910744d6ef8823dc2d22399406
Samet DOGAN <s.doqann1570@gmail.com>
2021-09-16 13:56:46 +0300

    Interfaceler Tanimlanmaya Baslandi. 14.Ders Sonu

---
+++ DataAccess/Abstract/ICategoryDal.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Entites.Concrete;
+
+namespace DataAccess.Abstract
+{
+    public interface ICategoryDal
+    {
+        List<Category> GetAll();
+        void Add(Category category);
+        void Delete(Category category);
+        void Update(Category category);
+        Category GetById(int id);
+    }
+}

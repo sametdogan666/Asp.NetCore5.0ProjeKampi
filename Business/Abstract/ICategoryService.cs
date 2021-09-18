73a671a9e874d8acbc236dba8719b7216dc40cca
Samet DOGAN <s.doqann1570@gmail.com>
2021-09-18 16:00:43 +0300

    Business Katmaninda Service Ve Managerlar Yazilmaya Baslandi. 17.Ders Sonu

---
+++ Business/Abstract/ICategoryService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Entites.Concrete;
+
+namespace Business.Abstract
+{
+    public interface ICategoryService
+    {
+        void Add(Category category);
+        void Delete(Category category);
+        void Update(Category category);
+        List<Category> GetAll();
+        Category GetById(int id);
+    }
+}
---
+++ Business/Concrete/CategoryManager.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Business.Abstract;
+using Entites.Concrete;
+
+namespace Business.Concrete
+{
+    public class CategoryManager : ICategoryService
+    {
+        public void Add(Category category)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(Category category)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Category category)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Category> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Category GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

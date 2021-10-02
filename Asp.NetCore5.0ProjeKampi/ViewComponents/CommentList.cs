4b8755ee38caf7a504826391951ff7603196235a
Samet DOGAN <s.doqann1570@gmail.com>
2021-10-02 08:20:54 +0300

    View Component. 30.Ders Sonu

---
+++ Asp.NetCore5.0ProjeKampi/ViewComponents/CommentList.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Asp.NetCore5._0ProjeKampi.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Asp.NetCore5._0ProjeKampi.ViewComponents
+{
+    public class CommentList : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            var commentValues = new List<UserComment>
+            {
+                new UserComment
+                {
+                    Id = 1,
+                    UserName = "Samet"
+                },
+                new UserComment
+                {
+                    Id = 2,
+                    UserName = "Enes"
+                }
+            };
+            return View(commentValues);
+        }
+    }
+}

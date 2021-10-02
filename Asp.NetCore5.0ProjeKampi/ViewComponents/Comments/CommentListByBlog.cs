c409df4bb1562a1f6a6ce899a04f850112f4d1da
Samet DOGAN <s.doqann1570@gmail.com>
2021-10-02 08:37:10 +0300

    Bloglara Ait Yorumlarin Listelenmesi. 31.Ders Sonu

---
+++ Asp.NetCore5.0ProjeKampi/ViewComponents/Comments/CommentListByBlog.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Business.Concrete;
+using DataAccess.Concrete.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Asp.NetCore5._0ProjeKampi.ViewComponents.Comments
+{
+    public class CommentListByBlog :ViewComponent
+    {
+        CommentManager _commentManager = new CommentManager(new EfCommentRepository());
+
+        public IViewComponentResult Invoke()
+        {
+            var results = _commentManager.GetAll(5);
+            return View(results);
+        }
+    }
+}

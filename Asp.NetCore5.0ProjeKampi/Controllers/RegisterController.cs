511a9f7374278744b9c428149f6eaf80ea479487
Samet DOGAN <s.doqann1570@gmail.com>
2021-10-05 21:32:33 +0300

    HttpGet Ve HttpPost Attributelari Ve Register Islemi Yapildi

---
+++ Asp.NetCore5.0ProjeKampi/Controllers/RegisterController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Asp.NetCore5._0ProjeKampi.Controllers
+{
+    public class RegisterController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Business/Concrete/WriterManager.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Business.Abstract;
+using DataAccess.Abstract;
+using Entites.Concrete;
+
+namespace Business.Concrete
+{
+    public class WriterManager : IWriterService
+    {
+        private IWriterDal _writerDal;
+
+        public WriterManager(IWriterDal writerDal)
+        {
+            _writerDal = writerDal;
+        }
+
+        public void Add(Writer writer)
+        {
+            _writerDal.Add(writer);
+        }
+    }
+}

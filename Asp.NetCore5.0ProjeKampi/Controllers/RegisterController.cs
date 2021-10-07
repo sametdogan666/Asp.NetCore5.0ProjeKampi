6cc622837f2ddb7e6f8f3c5deb9170192d5b70fb
Samet DOGAN <s.doqann1570@gmail.com>
2021-10-07 09:25:29 +0300

    Yazar Kaydi Ve Sweet Alert Kullanimi Gerceklestirildi. 34. Ders Sonu

--- Asp.NetCore5.0ProjeKampi/Controllers/RegisterController.cs
+++ Asp.NetCore5.0ProjeKampi/Controllers/RegisterController.cs
@@ -3,11 +3,16 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Business.Concrete;
+using DataAccess.Concrete.EntityFramework;
+using Entites.Concrete;
 
 namespace Asp.NetCore5._0ProjeKampi.Controllers
 {
     public class RegisterController : Controller
     {
+        private WriterManager _writerManager = new WriterManager(new EfWriterRepository());
+
         [HttpGet]
         public IActionResult Index()
         {
@@ -15,9 +20,12 @@ namespace Asp.NetCore5._0ProjeKampi.Controllers
         }
 
         [HttpPost]
-        public IActionResult Index()
+        public IActionResult Index(Writer writer)
         {
-            return View();
+            writer.WriterStatus = true;
+            writer.WriterAbout = "Deneme Test";
+            _writerManager.Add(writer);
+            return RedirectToAction("Index", "Blogs");
         }
     }
 }

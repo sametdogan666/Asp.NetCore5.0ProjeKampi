ec192ea5e9a32cc203b760252caafa2bb1c8c0e4
Samet DOGAN <s.doqann1570@gmail.com>
2021-10-10 00:03:36 +0300

    Mail Bulteni. 38.Ders Sonu

---
+++ Asp.NetCore5.0ProjeKampi/Controllers/NewsLettersController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Business.Concrete;
+using DataAccess.Concrete.EntityFramework;
+using Entites.Concrete;
+
+namespace Asp.NetCore5._0ProjeKampi.Controllers
+{
+    public class NewsLettersController : Controller
+    {
+        NewsLetterManager _newsLetterManager = new NewsLetterManager(new EfNewsLetterRepository());
+
+        [HttpGet]
+        public PartialViewResult SubscribeMail()
+        {
+            return PartialView();
+        }
+
+        [HttpPost]
+        public PartialViewResult SubscribeMail(NewsLetter newsLetter)
+        {
+            newsLetter.MailStatus = true;
+            _newsLetterManager.Add(newsLetter);
+            return PartialView();
+        }
+    }
+}
---
+++ Business/Concrete/NewsLetterManager.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Business.Abstract;
+using DataAccess.Abstract;
+using Entites.Concrete;
+
+namespace Business.Concrete
+{
+    public class NewsLetterManager : INewsLetterService
+    {
+        private INewsLetterDal _newsLetterDal;
+
+        public NewsLetterManager(INewsLetterDal newsLetterDal)
+        {
+            _newsLetterDal = newsLetterDal;
+        }
+
+
+        public void Add(NewsLetter newsLetter)
+        {
+           _newsLetterDal.Add(newsLetter);
+        }
+    }
+}

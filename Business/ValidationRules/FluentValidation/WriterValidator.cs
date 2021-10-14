ae6bbdc8745872a348c5bf16f5b93fcae0dd18a2
Samet DOGAN <s.doqann1570@gmail.com>
2021-10-15 02:50:16 +0300

    Writer Validasyonda Sifre Isterleri Guncellendi

--- Business/ValidationRules/FluentValidation/WriterValidator.cs
+++ Business/ValidationRules/FluentValidation/WriterValidator.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Data;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using Entites.Concrete;
 using FluentValidation;
@@ -13,11 +14,13 @@ namespace Business.ValidationRules.FluentValidation
     {
         public WriterValidator()
         {
+            Regex regex = new Regex("^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[$@!%*?+#&'()[=\"€])[A-Za-z\\d$@!%*?+#&'()[=\"€']{8,}");
             RuleFor(x => x.WriterName).NotEmpty().WithMessage("Yazar  adı soyadı kısmı boş geçilemez.");
             RuleFor(x => x.WriterMail).NotEmpty().WithMessage("Mail adresi boş geçilemez.");
             RuleFor(x => x.WriterPassword).NotEmpty().WithMessage("Şifre alanı boş geçilemez.");
             RuleFor(x => x.WriterName).MinimumLength(2).WithMessage("Lütfen en az 2 karakter girişi yapınız.");
             RuleFor(x => x.WriterName).MaximumLength(50).WithMessage("Lütfen en fazla 50 karakterlik giriş yapınız.");
+            RuleFor(x => x.WriterPassword).Matches(regex).WithMessage("Şifre en az bir küçük harf, bir büyük harf, 1 özel karakter ve 1 rakam içermelidir. Ve en az 8 karakter olmalıdır.");
         }
     }
 }

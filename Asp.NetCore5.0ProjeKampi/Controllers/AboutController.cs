d12395a8fa92f6a2d76f175101e38376f65473f1
Samet DOGAN <s.doqann1570@gmail.com>
2021-10-19 10:33:04 +0300

    About Sayfasina Baslandi

---
+++ Asp.NetCore5.0ProjeKampi/Controllers/AboutController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Business.Concrete;
+using DataAccess.Concrete.EntityFramework;
+
+namespace Asp.NetCore5._0ProjeKampi.Controllers
+{
+    public class AboutController : Controller
+    {
+        AboutManager _aboutManager = new AboutManager(new EfAboutRepository());
+        public IActionResult Index()
+        {
+            var results = _aboutManager.GetAll();
+            return View(results);
+        }
+
+        public PartialViewResult SocialMediaAbout()
+        {
+            return PartialView();
+        }
+    }
+}

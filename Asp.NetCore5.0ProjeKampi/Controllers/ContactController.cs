21a61cf7a2d0ee502dd85bd4917ea2d365340763
Samet DOGAN <s.doqann1570@gmail.com>
2021-10-19 11:00:13 +0300

    Contact Sayfasi Hazirlandi

---
+++ Asp.NetCore5.0ProjeKampi/Controllers/ContactController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Business.Concrete;
+using DataAccess.Concrete.EntityFramework;
+using Entites.Concrete;
+
+namespace Asp.NetCore5._0ProjeKampi.Controllers
+{
+    public class ContactController : Controller
+    {
+        ContactManager _contactManager = new ContactManager(new EfContactRepository());
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(Contact contact)
+        {
+            contact.ContactCreateDate = DateTime.Parse(DateTime.Now.ToShortDateString());
+            contact.ContactStatus = true;
+            _contactManager.Add(contact);
+            return RedirectToAction("Index","Blogs");
+        }
+    }
+}
